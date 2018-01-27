16a5f9ee73b58a17a75cc20b451010c33fb73bde
Jeremy <jeremygrondahl@gmail.com>
2018-01-27 05:19:30 -0600

    refactored html and setup paths in component. Alteredt gradient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  billowClouds = [{ class: 'cloud1', path: '../assets/BGCloud1.png', speed: 'x1' },
+    { class: 'cloud2', path: '../assets/BGCloud2.png', speed: 'x2'},
+    { class: 'cloud3', path: '../assets/BGCloud3.png', speed: 'x3'}];
+
+  darkClouds = [{ class: 'cloud1', path: '../assets/MGCloud1.png', speed: 'x4' },
+  { class: 'cloud2', path: '../assets/MGCloud2.png', speed: 'x5' },
+  { class: 'cloud3', path: '../assets/MGCloud3.png', speed: 'x6' }];
 }
